f9424272aaa6df4904c0ebaa1b3bf1edc3e1bfdf
Mirkoweitkowitz <99251369+Mirkoweitkowitz@users.noreply.github.com>
2022-09-02 11:11:43 +0200

    Layout für die views erstellt

---
+++ PraxisFreitag3/Bucket-ListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Bucket-ListViewController.swift
+//  PraxisFreitag3
+//
+//  Created by Mirko Weitkowitz on 02.09.22.
+//
+
+import UIKit
+
+class Bucket_ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
